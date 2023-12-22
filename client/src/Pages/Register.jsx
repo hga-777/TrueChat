8635472913f8722690f98ebbdcc5142d906b124e
Hritik Anand <realhga@gmail.com:--global user.email>
2023-12-22 13:59:52 +0530

    Toast added in regis and UserModel is created

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -1,15 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useNavigate, Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import axios from "axios";
 import logo from '../assets/logo2.svg';
+import "react-toastify/dist/ReactToastify.css";
 
 function Register() {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+
+  const handleChange = (event) => { setValues({ ...values, [event.target.name]: event.target.value });};
+
+  const handleValidation = () => {
+    const { password, confirmPassword, username, email } = values;
+    if (password !== confirmPassword) {
+      toast.error(
+        "Password and confirm password should be same.",
+        toastOptions
+      );
+      return false;
+    } else if (username.length < 3) {
+      toast.error(
+        "Username should be greater than 3 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (password.length < 8) {
+      toast.error(
+        "Password should be equal or greater than 8 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (email === "") {
+      toast.error("Email is required.", toastOptions);
+      return false;
+    }
+
+    return true;
+  };
+  
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert("form");
+    if(handleValidation()){
+      
+    }
   };
-  const handleChange = (event) => {};
-  return (
+  return (  
     <>
       <FormContainer>
         <form onSubmit={(event) => handleSubmit(event)}>
@@ -47,6 +95,7 @@ function Register() {
           </span>
         </form>
       </FormContainer>
+      <ToastContainer />
     </>
   );
 }
